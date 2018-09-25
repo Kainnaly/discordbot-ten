7796b136a285eff182d03cf339c29d5a4a5efae4
Léa Thibout <lea.thibout@eisti.fr>
2018-09-25 11:31:19 +0200

    @ ok

--- Ten-nii.js
+++ Ten-nii.js
@@ -1,46 +1,58 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 // const auth = require('./auth.json');
-var prefix = '*/';
+var prefix = "*/";
 
-bot.on('ready', () => {
+bot.on("ready", () => {
   console.log("Ohayoo. ");
-  bot.user.setActivity('Being angry to Riku.')
+  bot.user.setActivity("Being angry to Riku.");
 });
 
 
-bot.on('message', message => {
+bot.on("message", message => {
 
-    if (!message.content.startsWith(prefix)) return;
+    if ( (!message.content.startsWith(prefix)) || 
+        (!message.content.indexOf("<@492396386906800148>") > -1) ||
+        (!message.content.indexOf("@Ten-nii") > -1) ) return;
+    // 492396386906800148
+    // anderson : 424318724242407424
 
-    var args = message.content.slice(prefix.length).split(/ +/);
+    var args = message.content.slice(prefix.length).split("/ +/");
     var cmd = args.shift().toLowerCase();
 
     switch(cmd) {
         // !ping
-        case 'ping':
-            message.channel.send('Pong !')
+        case "ping":
+            message.channel.send("Pong !")
             // bot.sendMessage({
             //     to: channelID,
             //     message: 'Pong!'
             // });
         break;
 
-        case 'pong':
-            message.channel.send('Ping-')
+        case "pong":
+            message.channel.send("Ping-")
         break;
 
-        case 'help':
-            message.reply("besoin d'aide ? \nPour l'instant mes commandes sont : \nping : renvoi pong \npong : renvoi ping \ngao : gentil petit dinosaure \nhaikyuu : meme aléatoire d'Haikyuu!! \nvideo : envoi une vidéo de IDOLiSH7/TRIGGER/Re:vale \nnope : nope. ");
+        case "help":
+            var msgRep = "besoin d'aide ? \n"
+            msgRep += "Pour l'instant mes commandes sont : \n"
+            msgRep += "`ping` : renvoi pong \n"
+            msgRep += "`pong` : renvoi ping \n"
+            msgRep += "`gao` : gentil petit dinosaure \n"
+            msgRep += "`haikyuu` : meme aléatoire d'Haikyuu!! \n"
+            msgRep += "`video` : envoi une vidéo de IDOLiSH7/TRIGGER/Re:vale \n"
+            msgRep += "`nope` : nope. "
+            message.reply(msgRep);
         break;
 
-        case 'gao':
+        case "gao":
             message.channel.send("", {
                 file: "imgs/Gao.jpg"
             });
         break;
 
-        case 'haikyuu':
+        case "haikyuu":
             // fso = new ActiveXObject("Scripting.FileSystemObject");
             // var n = 0;
             // rep = "imgs/memes/memesHaikyuu"
@@ -67,18 +79,18 @@ bot.on('message', message => {
         break;
 
 
-        case 'video':
+        case "video":
             //Diamond Fusion 3 fois parce que c'est la meilleure
-            var videos = ['https://youtu.be/Cj17Lh3QAM8',
-                'https://youtu.be/VLNZVGDp9jg',
-                'https://youtu.be/WMBdSdUKo6Y',
-                'https://youtu.be/WMBdSdUKo6Y',
-                'https://youtu.be/WMBdSdUKo6Y',
-                'https://youtu.be/aoCPwWt_wr4',
-                'https://youtu.be/yacHD29uSVc',
-                'https://youtu.be/DBSZIt9z9eg',
-                'https://youtu.be/kkIWH6dHRYE',
-                'https://youtu.be/stz_2EpFvgE'
+            var videos = ["https://youtu.be/Cj17Lh3QAM8",
+                "https://youtu.be/VLNZVGDp9jg",
+                "https://youtu.be/WMBdSdUKo6Y",
+                "https://youtu.be/WMBdSdUKo6Y",
+                "https://youtu.be/WMBdSdUKo6Y",
+                "https://youtu.be/aoCPwWt_wr4",
+                "https://youtu.be/yacHD29uSVc",
+                "https://youtu.be/DBSZIt9z9eg",
+                "https://youtu.be/kkIWH6dHRYE",
+                "https://youtu.be/stz_2EpFvgE"
                 ];
 
             var n = videos.length;
@@ -89,12 +101,15 @@ bot.on('message', message => {
         
         break;
 
-        case 'nope':
+        case "nope":
             message.channel.send("Yada.", {
                 file: "imgs/nope.jpg"
             })
         break;
 
+        // case 'zerochan':
+        //     var ze = "https://www.zerochan.net/"
+        //     message.channel.send(ze+"")
 
     }
 });
